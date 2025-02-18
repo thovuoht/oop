b60bea7227b369587d6dc7e8dfb96f96b7c57ca1
Vũ Tiến Thọ <thovtph52262@gmail.com>
2025-02-19 05:27:59 +0700

    up

--- app/Controller.php
+++ app/Controller.php
@@ -7,8 +7,6 @@ class Controller
     public function validate($validator, $data, $rules)
     {
         $validation = $validator->make($data, $rules);
-
-        // then validate
         $validation->validate();
 
         if ($validation->fails()) {
@@ -24,27 +22,22 @@ class Controller
 
         $message = date('d-m-Y H:i:s') . ' - ' . $message . PHP_EOL;
 
-        // Type: 3 - Ghi vào file
         error_log($message, 3, "storage/logs/$date.log");
     }
 
     public function uploadFile(array $file, $folder = null)
     {
-        // Thông tin về file
-        $fileTmpPath = $file['tmp_name']; // Đường dẫn tạm thời của file
-        $fileName = time() . '-' . $file['name']; // Tên file chống trùng bằng timestamp
 
+        $fileTmpPath = $file['tmp_name'];
+        $fileName = time() . '-' . $file['name']; 
         $uploadDir = 'storage/uploads/' . $folder . '/';
 
-        // Tạo thư mục nếu chưa tồn tại
         if (!is_dir($uploadDir)) {
             mkdir($uploadDir, 0755, true);
         }
 
-        // Đường dẫn đầy đủ để lưu file
         $destPath = $uploadDir . $fileName;
 
-        // Di chuyển file từ thư mục tạm thời đến thư mục đích
         if (move_uploaded_file($fileTmpPath, $destPath)) {
             return $destPath;
         }
